4a68c99a49238d60fb9df5ab0cf6ba1d5dacfd80
Vincent <vincenttafolla1@gmail.com>
2019-06-13 10:13:38 -0500

    first commit

--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace WeatherApp
 {
@@ -6,7 +8,18 @@ namespace WeatherApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string apiKey = System.IO.File.ReadAllText("api-key.txt");
+
+            // API Key usage example
+            // http://api.openweathermap.org/data/2.5/forecast?id=524901&AAPID={APIKEY}
+
+            string apiCall = "http://api.openweathermap.org/data/2.5/weather?zip=94040,us" + "&APPID=" + apiKey;
+
+            HttpClient httpClient = new HttpClient();
+            Task<string> response = httpClient.GetStringAsync(apiCall);
+            response.Wait();
+            Console.WriteLine(response.Result);
+            
         }
     }
 }
